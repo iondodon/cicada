969342448f9059d399fa50810f2b033d682417ab
Ion Dodon <iondodon2@gmail.com>
2019-05-05 16:04:42 +0300

    nginx configured

---
+++ reactjs/src/pages/homePageComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+
+class Home extends React.Component {
+
+    exampleMethod(){
+        console.log("js is running");
+    }
+
+    head(){
+        return(
+            <Helmet>
+                <title>
+                    My page title
+                </title>
+            </Helmet>
+        )
+    }
+
+    render() {
+        return(
+            <div>
+                {this.head()}
+                <h1>
+                    My home page
+                </h1>
+                <p>
+                    Some content
+                </p>
+                <button onClick={this.exampleMethod}>Console log some text</button>
+            </div>
+        )
+    }
+}
+
+export default Home;
