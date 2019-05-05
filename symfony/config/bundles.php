381a41b8cf1ee81fca5e0c3e15ee68affa4ccd5e
Ion Dodon <iondodon2@gmail.com>
2019-05-05 23:36:47 +0300

    User Entity created

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -3,4 +3,10 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
---
+++ symfony/src/Controller/BaseController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class BaseController
+ * @package App\Controller
+ *
+ * @Route("/api", name="api")
+ */
+class BaseController extends AbstractController
+{
+    /**
+     * @return JsonResponse
+     * @Route("/ion", name="ion")
+     */
+    public function index()
+    {
+        return JsonResponse::create(["ion" => "noi"]);
+    }
+}
