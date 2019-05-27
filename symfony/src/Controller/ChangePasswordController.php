0b4f45404d11bbb761548c445ccd3aef84a62d02
Ion Dodon <iondodon2@gmail.com>
2019-05-27 18:49:16 +0300

    change password

--- symfony/src/Controller/ChangePasswordController.php
+++ symfony/src/Controller/ChangePasswordController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -17,22 +16,43 @@ class ChangePasswordController extends FOSRestController
 {
     /**
      * @Route(path="api/changePassword", name="change_password")
+     * @Method("POST")
+     * @param Request $request
+     * @return JsonResponse
      */
     public function postChangePasswordAction(Request $request): JsonResponse
     {
-        $user = new User();
-        $form = $this->createForm(ChangePasswordType::class, $user);
+        $form = $this->createForm(ChangePasswordType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $email = $request->request->get('email');
-            $password = $form->getData()->getPassword();
+            $old_plain_password = $request->request->get('old_password');
+            $new_plain_password = $request->request->get('new_password');
+
+
+            $user = $this->getDoctrine()->getRepository(User::class)
+                ->findOneBy(['email'=> $email]);
+
+            if (!$user) {
+                throw $this->createNotFoundException();
+            }
+
+            $isValid = $this->get('security.password_encoder')
+                ->isPasswordValid($user, $old_plain_password);
+
+            if(!$isValid) {
+                throw $this->createAccessDeniedException();
+            }
+
             $passwordNew = $this->get('security.password_encoder')
-                               ->encodePassword($user, $user->getPassword());
+                               ->encodePassword($user, $new_plain_password);
+
             $em = $this->getDoctrine()->getManager();
             $userRepository = $em->getRepository(User::class)->findOneBy(['email' => $email]);
             $userRepository->setPassword($passwordNew);
 
+            /** @var User $userRepository */
             $event = new EmailChangePasswordEvent($userRepository);
             $dispatcher = $this->get('event_dispatcher');
 			$dispatcher->dispatch(EmailChangePasswordEvent::NAME, $event);
