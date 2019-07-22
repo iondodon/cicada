eb86c3443a124ff5b4e362e64ddd1e2bf0f15183
Ion Dodon <iondodon2@gmail.com>
2019-07-22 23:08:45 +0300

    topmenu - logo

--- reactjs/components/TopMenu.js
+++ reactjs/components/TopMenu.js
@@ -14,34 +14,47 @@ class TopMenu extends React.Component {
 
     render(){
         return (
-            <div className="menu top-menu">
-                <Link href={"/"}>
-                    <a className="menu-item">Home</a>
-                </Link>{' '}
+            <div className="alert alert-success">
+                <div className="menu top-menu">
+                    <Link href={"/"}>
+                        <a className="menu-item">Home</a>
+                    </Link>{' '}
 
-                |
-                <Link href={"/login"}>
-                    <a className="menu-item">Login</a>
-                </Link>{' '}
+                    |
+                    <Link href={"/login"}>
+                        <a className="menu-item">Login</a>
+                    </Link>{' '}
 
-                |
+                    |
 
-                <Link href={"/signup"}>
-                    <a className="menu-item">SignUp</a>
-                </Link>{' '}
+                    <Link href={"/signup"}>
+                        <a className="menu-item">SignUp</a>
+                    </Link>{' '}
 
-                |
-                <a className="menu-item">About</a>
+                    |
+                    <a className="menu-item">About</a>
+                </div>
+
+                <img src={'../static/cicada.png'} className={"logo"}  alt="true" />
 
                 { /*language=SCSS*/ }
                 <style jsx>{`
-                  .top-menu {
-                    display: flex;
-                    flex-direction: row;
-                    justify-content: center;
-                    margin-bottom: 1rem;
-                  }
-                `}
+                      .top-menu {
+                        display: flex;
+                        flex-direction: row;
+                      }
+                      
+                      .alert {
+                        display: flex;
+                        flex-direction: row;
+                        margin-top: 1rem;
+                        overflow: hidden;
+                      }
+                      
+                      .logo {
+                        height: 10rem;
+                      }
+                    `}
                 </style>
             </div>
         );
