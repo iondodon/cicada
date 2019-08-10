38c4857a4576e394cd1a39254cce3d67b7f6c79a
Ion Dodon <iondodon2@gmail.com>
2019-08-10 19:45:51 +0300

    CreateTeam : add new user in state

--- reactjs/components/TeamForm.js
+++ reactjs/components/TeamForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import '../i18n';
 import { withNamespaces } from 'react-i18next';
+import config from "../configs/keys";
 
 
 class TeamForm extends React.Component {
@@ -11,10 +12,12 @@ class TeamForm extends React.Component {
         this.t = t;
 
         this.state = {
+            newMemberUsername: "",
             members: ["iondodon"]
         };
 
-        this.searchUser = this.searchUser.bind(this);
+        this.addNewMember = this.addNewMember.bind(this);
+        this.closeError = this.closeError.bind(this);
     }
 
     componentDidMount() {
@@ -29,9 +32,48 @@ class TeamForm extends React.Component {
         });
     }
 
-    searchUser() {
+    async addNewMember() {
+        const request = {
+            method: 'GET',
+            mode: 'cors',
+            credentials: "include"
+        };
 
-        return null;
+        try {
+            let response = await fetch(config.API_URL + '/api/users/exists/' + this.state.newMemberUsername, request);
+
+            if (response.status === 401) {
+                document.getElementsByClassName('error-content')[0].innerHTML = 'Unauthorized.';
+                document.getElementsByClassName('alert-error')[0].setAttribute('style', 'display: inline;');
+            } else if (response.status === 204) {
+                document.getElementsByClassName('error-content')[0].innerHTML = 'Such user doesn\'t exist.';
+                document.getElementsByClassName('alert-error')[0].setAttribute('style', 'display: inline;');
+            } else if (response.status === 302) {
+
+                const getUsernameFromState = () => {
+                    return this.state.members.find((memberUsername) => {
+                        return memberUsername === this.state.newMemberUsername;
+                    })
+                };
+                
+                if(!getUsernameFromState()) {
+                    await this.setState( { members: [...this.state.members, this.state.newMemberUsername ] } );
+                } else {
+                    document.getElementsByClassName('error-content')[0].innerHTML = this.state.newMemberUsername + ' is already a member.';
+                    document.getElementsByClassName('alert-error')[0].setAttribute('style', 'display: inline;');
+                }
+            } else {
+                document.getElementsByClassName('error-content')[0].innerHTML = 'Unexpected error.';
+                document.getElementsByClassName('alert-error')[0].setAttribute('style', 'display: inline;');
+            }
+        } catch (e) {
+            document.getElementsByClassName('error-content')[0].innerHTML += e.message;
+            document.getElementsByClassName('alert-error')[0].setAttribute('style', 'display: inline;');
+        }
+    }
+
+    closeError(e) {
+        e.target.parentElement.setAttribute('style', 'display: none;');
     }
 
     render(){
@@ -73,21 +115,28 @@ class TeamForm extends React.Component {
                             <td className={"td-content-center"}>
                                 <fieldset className="form-group">
                                     <input
-                                        id="username"
                                         type="text"
                                         placeholder="username..."
                                         className={"form-control username-input"}
+                                        onChange={async (event) => {
+                                            await this.setState( { newMemberUsername: event.target.value } );
+                                        }}
                                     />
-                                    <div className="help-block">...</div>
                                 </fieldset>
                             </td>
                             <td>
-                                <button className="btn btn-success btn-ghost btn-block">add</button>
+                                <button className="btn btn-success btn-ghost btn-block" onClick={this.addNewMember}>add</button>
                             </td>
                         </tr>
 
                     </tbody>
                 </table>
+
+
+                <div className="alert alert-error" style={{ display: 'none' }} >
+                    <div className={"error-content"} >Error message</div>
+                    {'\u00A0'} <a onClick={this.closeError}>x</a>
+                </div>
 
                 <button className="btn btn-success btn-block">Save</button>
 
@@ -124,6 +173,15 @@ class TeamForm extends React.Component {
                     text-align: center;
                     border: none;
                   } 
+                  
+                  .alert {
+                        display: flex;
+                        flex-direction: row;
+                        text-align: center;
+                        margin-top: 2rem;
+                        margin-bottom: 0;
+                        justify-content: center;
+                  }
                 `}
                 </style>
             </div>
