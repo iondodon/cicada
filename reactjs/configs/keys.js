b7a9ac8617c692e3a42520503007e505d7baa928
Io Dodon <iondodon2@gmail.com>
2020-05-15 07:47:40 +0300

    ssl

--- reactjs/configs/keys.js
+++ reactjs/configs/keys.js
@@ -1,5 +1,5 @@
 export default {
-    API_URL: 'https://cicadapuzzle.com:443',
+    API_URL: 'https://cicadapuzzle.com:9000',
     DOMAIN: 'cicadapuzzle.com',
     CAPTCHA_KEY: '6Leb8-QUAAAAANsfBh3CEHi6hWyaLzmyF5oxyoTd'
 }
