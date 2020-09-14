74560facd0e40435d6815e1339f05e688956c698
Ion Dodon <iondodon2@gmail.com>
2020-09-14 19:07:13 +0300

    switched to localhost with no ssl

--- reactjs/configs/keys.js
+++ reactjs/configs/keys.js
@@ -1,5 +1,5 @@
 export default {
-    API_URL: 'https://localhost:9000',
+    API_URL: 'http://localhost:9000',
     DOMAIN: 'localhost',
     CAPTCHA_KEY: '6Leb8-QUAAAAANsfBh3CEHi6hWyaLzmyF5oxyoTd'
 }
